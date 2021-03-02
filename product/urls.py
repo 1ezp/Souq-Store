66d28c5e7f58a43431176b634fc0f2e6da555bb2
hcczz <hekmt111@gmail.com>
2021-03-02 23:21:23 +0300

    Edit

--- product/urls.py
+++ product/urls.py
@@ -2,7 +2,9 @@
 from . import views
 from django.urls import path
 
+app_name = 'product'
+
 urlpatterns = [
     path('',views.product_list),
-    path('<int:id>', views.product_detail),
+    path('<int:id>', views.product_detail, name = "product_detail"),
 ] 
--- product/views.py
+++ product/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 # Create your views here.
 
 def product_list(request):
-    product_list = Product.objects.all()
-    context = {'product_list':product_list}
+    product_list = Product.objects.get_queryset().order_by('id')
+    paginator = Paginator(product_list, 3)
 
+    page_number = request.GET.get('page')
+    product_list = paginator.get_page(page_number)
+    context = {'product_list':product_list}
     return render(request,'Product/product_list.html', context)
 
 def product_detail(request,id):
